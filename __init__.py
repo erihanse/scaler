e0e28465ad5200e078393047d7f08ce9092f5f7b
Erik Aaron Hansen <erihanse@outlook.com>
2017-05-18 21:54:59 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,16 @@
+'''
+Package auto_scaler
+'''
+
+from auto_scaler import AutoScaler
+
+__all__ = ['auto_scaler']
+
+
+# if __name__ == "__main__":
+#     # Create service
+#     WEBAUTOSCALER = AutoScaler(
+#         image_name="erihanase/php-web:latest",
+#         service_name="php-web-service",
+#         placement_constraints=['node.role==worker']
+#     )
---
+++ auto_scaler.py
@@ -0,0 +1,43 @@
+'''
+Module auto_scaler
+'''
+
+import docker
+
+class AutoScaler(object):
+    '''
+    Contains properties and methods necessary for scaling a service in Docker.
+    '''
+    def __init__(self, image_name, service_name, placement_constraints):
+        self.image_name = image_name
+        self.service_name = service_name
+        self.placement_constraints = placement_constraints
+        self.client = docker.from_env()
+
+    def get_service(self, service_name):
+        '''
+        Returns the service this @AutoScaler runs for.
+        '''
+        return self.client.services.list(
+            filters={'name':service_name}
+        )[0]
+
+    def scale_service(self, new_service_replica_count):
+        '''
+        Scales the service so that the service has @new_service_replica_count
+        amount of containers.
+        '''
+        service = self.get_service(self.service_name)
+        service.update(
+            image=self.image_name,
+            name=self.service_name,
+            constraints=self.placement_constraints,
+            mode={'replicated':{'replicas':new_service_replica_count}}
+        )
+
+    def run_auto_scaler(self, poll_interval):
+        '''
+        Runs the auto-scaler until the program is stopped. The auto-scaler
+        updates every @poll_interval seconds.
+        '''
+        pass
