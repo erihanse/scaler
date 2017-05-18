6e0b2b6459c40754b703a37629766505dfa81067
Erik Aaron Hansen <erihanse@outlook.com>
2017-05-18 23:30:56 +0200

    update

--- __init__.py
+++ __init__.py
@@ -1,16 +1,9 @@
 '''
 Package auto_scaler
 '''
-
-from . import AutoScaler
+from auto_scaler import AutoScaler
 
 __all__ = ['auto_scaler']
+__version__ = 1
 
 
-# if __name__ == "__main__":
-#     # Create service
-#     WEBAUTOSCALER = AutoScaler(
-#         image_name="erihanase/php-web:latest",
-#         service_name="php-web-service",
-#         placement_constraints=['node.role==worker']
-#     )
--- __main__.py
+++ __main__.py
@@ -1,3 +1,17 @@
+'''
+If package is to be run. This will scale the service until terminated.
+'''
+
 from auto_scaler import AutoScaler
 
-print "hei"
+def main():
+    WEBAUTOSCALER = AutoScaler(
+        image_name="erihanase/php-web:latest",
+        service_name="php-web-service",
+        placement_constraints=['node.role==worker']
+    )
+
+
+
+if __name__ == "__main__":
+    main()
